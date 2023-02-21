861f088f8a1f5a7a0ba990d6b25b8464319a4122
Inna-Kovalishina <114028931+Inna-Kovalishina@users.noreply.github.com>
2023-02-21 07:52:44 +0200

    додає скрипт перемикача кольорів

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const bodyEl = document.querySelector('body');
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
 
+btnStart.addEventListener('click', onBtnStartClick);
+btnStop.addEventListener('click', onBtnStopClick);
+
+let timerId = null;
+
+function onBtnStartClick() {
+  timerId = setInterval(() => {
+      bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000)
+
+  btnStart.setAttribute('disabled', 'disabled');
+}
+
+
+function onBtnStopClick() {
+  clearInterval(timerId);
+
+  btnStart.removeAttribute('disabled');
+}
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
